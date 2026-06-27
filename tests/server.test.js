4f7a2fd3d45f41bf1547f714f39fcc2706c7931a
JSDenis <den.sanches@mail.ru>
2026-06-27 12:33:18 +0300

    update test

--- tests/server.test.js
+++ tests/server.test.js
@@ -5,7 +5,7 @@ describe('GET /datalab5', () => {
     it('should return Lab 5 data.', async () => {
         const response = await request(app).get('/datalab5');
         expect(response.status).toBe(200);
-        expect(response.text).toBe('Lab 5 data.123');
+        expect(response.text).toBe('Lab 5 data.1234');
     });
 });
  
